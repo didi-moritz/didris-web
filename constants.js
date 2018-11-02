1e0723d43c97ab5e5c6e0553ad3b13c34214ad94
whdimori <dietmar.moritz@willhaben.at>
2018-11-02 10:43:00 +0100

    add first interaction

--- constants.js
+++ constants.js
@@ -1,3 +1,8 @@
 var constants = {
-    BLOCK_IMAGE: "./img/block.png"
+    BLOCK_IMAGE: "./img/block.png",
+    BLOCK_SIZE: 12,
+    KEY_UP: 38,
+    KEY_DOWN: 40,
+    KEY_LEFT: 37,
+    KEY_RIGHT: 39
 };
--- main.js
+++ main.js
@@ -4,9 +4,38 @@ var didris = (function() {
 
     let block = null;
 
+    let position = {
+        x: 0,
+        y: 0
+    };
+
     function init() {
         initView();
         initSprites();
+        initControls();
+    }
+
+    function initControls() {
+        document.addEventListener("keydown", onKeyPress);
+    }
+
+    function onKeyPress(event) {
+        switch (event.keyCode) {
+            case constants.KEY_RIGHT:
+                position.x++;
+                break;
+            case constants.KEY_LEFT:
+                position.x--;
+                break;
+            case constants.KEY_UP:
+                position.y--;
+                break;
+            case constants.KEY_DOWN:
+                position.y++;
+                break;
+        }
+
+        updateBlock();
     }
 
     function initSprites() {
@@ -14,9 +43,12 @@ var didris = (function() {
             .add(constants.BLOCK_IMAGE)
             .load(() => {
                 block = new PIXI.Sprite(PIXI.loader.resources[constants.BLOCK_IMAGE].texture);
-
                 app.stage.addChild(block);
             });
+    }
+
+    function updateBlock() {
+        block.position.set(position.x * constants.BLOCK_SIZE, position.y * constants.BLOCK_SIZE);
     }
 
     function initView() {
